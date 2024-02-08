108d4e0efbdb5c200bcabb380f53d9d3ef63425d
kiol <kioly.vikerg0412@gmail.com>
2024-02-08 10:42:42 +0800

    work0208

--- blog/app/posts/[postId]/page.tsx
+++ blog/app/posts/[postId]/page.tsx
@@ -8,13 +8,13 @@ import html from 'remark-html'
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
 const postsDirectory = path.join(process.cwd(), 'src/posts');
+const fileNames = fs.readdirSync(postsDirectory);
 
 function getAllPostIds(){
-    const fileNames = fs.readdirSync(postsDirectory);
     return fileNames.map((fileName)=>{
         return {
             params: {
-                postId: fileName.replace(/\.md$/, ''),
+                postId: fileName.replace(/\.md$/, '')
             },
         }
     });
@@ -28,36 +28,45 @@ export async function getStaticPaths(){
     };
 }
 
-export default function blog({
-    params,
-}:{
-    params: {
-        postId:string,
-        contentHtml: string,
-        data: string,
+export async function getData(fileName: string) {
+    const fullpath = path.join(postsDirectory, `${fileName}.md`);
+    const fileContents = fs.readFileSync(fullpath, 'utf8');
+    const matterResult = matter(fileContents);
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+    return{
+        fileName,
+        contentHtml,
+        ...matterResult.data,
     };
+}
+
+export async function getDatas({params}:{
+    params:{
+        postId: string,
+    },
+}) {
+    const postData = await getData(params.postId);
+    return {
+        postData,
+    }
+}
+
+export default async function blog({
+    params
+}:{
+    params:{
+        postId: string,
+    },
 }){
-    const fileNames = fs.readdirSync(postsDirectory);
-    // console.log(fileNames);
-    var postDatas: string[] = [];
-    fileNames.map(async (fileName) => {
-        const fullpath = path.join(postsDirectory, `${fileName}`);
-        const fileContents = fs.readFileSync(fullpath, 'utf8');
-        // const matterResult = matter(fileContents);
-        // const processedContent = await remark()
-        //     .use(html)
-        //     .process(matterResult.content);
-        // const contentHtml = processedContent.toString();
-        postDatas.push(fileContents);
-    });
-    console.log(postDatas);
+    const {postData} = await getDatas({params});
     return (
         <div>
-            <h1>{params.postId}</h1>
-            {/* <p>{params.data}</p> */}
+            <h1>{postData.fileName}</h1>
             <br></br>
-            {/* <p>{params.contentHtml}</p> */}
-            <p>{postDatas[0]}</p>
+            <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
         </div>
     );
 }
